423577e48e0d436cdfb45e86f96d706e508becda
Baayri <m.baayri@gmail.com>
2021-05-15 02:40:26 +0300

    15.05.2021

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " Added");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " Deleted");
+        }
+
+        public void List(Customer[] customers)
+        {
+            int i = 1;
+            foreach ( Customer customer in customers)
+            {
+                Console.WriteLine(i + " - " + customer.FirstName + " " + customer.LastName);
+                i++;
+            }
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,33 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.CustomerNumber = "101";
+            customer1.FirstName = "Muhammed";
+            customer1.LastName = "Bayri";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.CustomerNumber = "102";
+            customer2.FirstName = "Customer";
+            customer2.LastName = "2";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.CustomerNumber = "103";
+            customer3.FirstName = "Customer";
+            customer3.LastName = "3";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer1);
+
+            Console.WriteLine("---------------------");
+
+            Customer[] customers = new Customer[] {customer1, customer2, customer3 };
+            customerManager.List(customers);
+
         }
     }
 }
